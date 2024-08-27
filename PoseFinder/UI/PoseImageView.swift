fcb2e1974cce130a3f7e0eba05a606b90fe6ef71
tujing <tujing_@hotmail.com>
2024-08-27 21:54:53 +0900

    wip: 先生と生徒のカラーの変更

--- PoseFinder/UI/PoseImageView.swift
+++ PoseFinder/UI/PoseImageView.swift
@@ -1,21 +1,25 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-Implementation details of a view that visualizes the detected poses.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ Implementation details of a view that visualizes the detected poses.
+ */
 
 import UIKit
 
 @IBDesignable
 class PoseImageView: UIImageView {
-
+    
+    // 教師と生徒の色を定義
+    let teacherColor = UIColor.blue
+    let studentColor = UIColor.red
+    
     /// A data structure used to describe a visual connection between two joints.
     struct JointSegment {
         let jointA: Joint.Name
         let jointB: Joint.Name
     }
-
+    
     /// An array of joint-pairs that define the lines of a pose's wireframe drawing.
     static let jointSegments = [
         // The connected joints that are on the left side of the body.
@@ -34,7 +38,7 @@ class PoseImageView: UIImageView {
         JointSegment(jointA: .leftShoulder, jointB: .rightShoulder),
         JointSegment(jointA: .leftHip, jointB: .rightHip)
     ]
-
+    
     /// The width of the line connecting two joints.
     @IBInspectable var segmentLineWidth: CGFloat = 2
     /// The color of the line connecting two joints.
@@ -43,9 +47,9 @@ class PoseImageView: UIImageView {
     @IBInspectable var jointRadius: CGFloat = 4
     /// The color of the circles drawn for each joint.
     @IBInspectable var jointColor: UIColor = UIColor.systemPink
-
+    
     // MARK: - Rendering methods
-
+    
     /// Returns an image showing the detected poses.
     ///
     /// - parameters:
@@ -60,58 +64,50 @@ class PoseImageView: UIImageView {
         
         let dstImageSize = CGSize(width: frame.width, height: frame.height)
         let dstImageFormat = UIGraphicsImageRendererFormat()
-
+        
         dstImageFormat.scale = 1
         let renderer = UIGraphicsImageRenderer(size: dstImageSize,
                                                format: dstImageFormat)
-
+        
         let dstImage = renderer.image { rendererContext in
             // Draw the current frame as the background for the new image.
             if isFrameDraw {
                 draw(image: frame, in: rendererContext.cgContext)
             }
-
-            // scaledPose の処理
-            for segment in PoseImageView.jointSegments {
-                let jointA = scaledPose[segment.jointA]
-                let jointB = scaledPose[segment.jointB]
-                
-                guard jointA.isValid, jointB.isValid else {
-                    continue
-                }
-                
-                drawLine(from: jointA,
-                         to: jointB,
-                         in: rendererContext.cgContext)
-            }
-
-            for joint in scaledPose.joints.values.filter({ $0.isValid }) {
-                draw(circle: joint, score: joint.score, in: rendererContext.cgContext)
-            }
-
-            // studentPose の処理
-            for segment in PoseImageView.jointSegments {
-                let jointA = studentPose[segment.jointA]
-                let jointB = studentPose[segment.jointB]
-                
-                guard jointA.isValid, jointB.isValid else {
-                    continue
-                }
-                
-                drawLine(from: jointA,
-                         to: jointB,
-                         in: rendererContext.cgContext)
-            }
-
-            for joint in studentPose.joints.values.filter({ $0.isValid }) {
-                draw(circle: joint, score: joint.score, in: rendererContext.cgContext)
-            }
+            
+            // 教師の姿勢を描画
+            drawPose(pose: scaledPose, in: rendererContext.cgContext, color: teacherColor)
+            
+            // 生徒の姿勢を描画
+            drawPose(pose: studentPose, in: rendererContext.cgContext, color: studentColor)
+            
         }
-
+        
         image = dstImage
         //print(image)
+        
     }
-
+    
+    
+    // 新しいメソッド
+    private func drawPose(pose: Pose, in context: CGContext, color: UIColor) {
+        for segment in PoseImageView.jointSegments {
+            let jointA = pose[segment.jointA]
+            let jointB = pose[segment.jointB]
+            
+            guard jointA.isValid, jointB.isValid else {
+                continue
+            }
+            
+            drawLine(from: jointA, to: jointB, in: context, color: color)
+        }
+        
+        for joint in pose.joints.values.filter({ $0.isValid }) {
+            let jointColor = colorBasedOnScore(joint.score)
+            draw(circle: joint, color: jointColor, in: context)
+        }
+    }
+    
     /// Vertically flips and draws the given image.
     ///
     /// - parameters:
@@ -128,7 +124,7 @@ class PoseImageView: UIImageView {
         cgContext.draw(image, in: drawingRect)
         cgContext.restoreGState()
     }
-
+    
     /// Draws a line between two joints.
     ///
     /// - parameters:
@@ -137,31 +133,31 @@ class PoseImageView: UIImageView {
     ///     - cgContext: The rendering context.
     func drawLine(from parentJoint: Joint,
                   to childJoint: Joint,
-                  in cgContext: CGContext) {
-        cgContext.setStrokeColor(segmentColor.cgColor)
+                  in cgContext: CGContext,
+                  color: UIColor) {
+        cgContext.setStrokeColor(color.cgColor)
         cgContext.setLineWidth(segmentLineWidth)
         //print(parentJoint.position)
         cgContext.move(to: parentJoint.position)
         cgContext.addLine(to: childJoint.position)
         cgContext.strokePath()
     }
-
+    
     /// Draw a circle in the location of the given joint.
     ///
     /// - parameters:
     ///     - circle: A valid joint whose position is used as the circle's center.
     ///     - cgContext: The rendering context.
-    private func draw(circle joint: Joint, score: Double, in cgContext: CGContext) {
-        // スコアに基づいて色を決定
-        let color = colorBasedOnScore(score)
+    private func draw(circle joint: Joint, color: UIColor, in cgContext: CGContext) {
+        
         cgContext.setFillColor(color.cgColor)
-
+        
         let rectangle = CGRect(x: joint.position.x - jointRadius, y: joint.position.y - jointRadius,
                                width: jointRadius * 2, height: jointRadius * 2)
         cgContext.addEllipse(in: rectangle)
         cgContext.drawPath(using: .fill)
     }
-
+    
     // スコアに基づいて色を決定する補助関数
     private func colorBasedOnScore(_ score: Double) -> UIColor {
         // スコアが0から1の範囲であると仮定
